c9cf28885864ce48a4784431a2e31ba5ba64f116
abasghlipour <142974582+abasghlipour@users.noreply.github.com>
2024-01-02 10:40:09 +0330

    add CI/CD

--- eshop_project/settings.py
+++ eshop_project/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3pmha18u*m0rlrq-n@we(s7gez2ag1*ozlf0_3u_l9@cj5+^en'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Application definition
 
